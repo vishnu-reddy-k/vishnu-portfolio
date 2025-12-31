639f57fe8d2cdeb024039ba03dc3e65d4d1dd8ca
vishnu-reddy-k <vishnuvrkarnati@gmail.com>
2026-01-01 03:11:35 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -135,8 +135,8 @@ export default function Portfolio() {
               Contact Me <ArrowRight size={18} className="group-hover:translate-x-1 transition-transform"/>
             </a>
             <div className="flex gap-4">
-              <SocialIcon icon={<Github size={20}/>} href="https://github.com" />
-              <SocialIcon icon={<Linkedin size={20}/>} href="https://linkedin.com" />
+              <SocialIcon icon={<Github size={20}/>} href="https://github.com/vishnu-reddy-k/" />
+              <SocialIcon icon={<Linkedin size={20}/>} href="https://www.linkedin.com/in/reddykv/" />
               <SocialIcon icon={<Mail size={20}/>} href={`mailto:${personalInfo.email}`} />
             </div>
           </motion.div>
